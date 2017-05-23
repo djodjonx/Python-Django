e63401977b80f1746799d88c88f720cd5ea7fefe
Djodjonx <djo.moutier@gmail.com>
2017-05-23 11:43:05 +0200

    join vueJs project with Python djangorest project

--- student_list/students/serializers.py
+++ student_list/students/serializers.py
@@ -5,6 +5,4 @@ from students.models import Student
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = ('id', 'first_name', 'last_name', 'age')
-
-    
+        fields = ('id', 'first_name')
